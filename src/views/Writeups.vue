1dcf2bc4ef54a8a7a7ff720c285020fa2b790fd7
beafn28 <beafn23@gmail.com>
2025-06-22 22:24:32 +0200

    writeups labs portswigger

--- src/views/Writeups.vue
+++ src/views/Writeups.vue
@@ -69,7 +69,7 @@ import { ref, computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const writeups = ref([
-	  { title: 'Laboratorios PortSwigger', platform: 'PortSwigger', difficulty: 'Fácil', link: 'https://beafn28.gitbook.io/beafn28/web-security/laboratorios-portswigger' },
+	  { title: 'Laboratorios PortSwigger', platform: 'PortSwigger', os: 'Windows/Linux', difficulty: 'Fácil', link: 'https://beafn28.gitbook.io/beafn28/web-security/laboratorios-portswigger' },
   { title: 'Brooklyn Nine Nine', platform: 'TryHackMe', os: 'Linux', difficulty: 'Fácil', link: 'https://beafn28.gitbook.io/beafn28/writeups/tryhackme/brooklyn-nine-nine' },
   { title: 'Pickle Rick', platform: 'TryHackMe', os: 'Linux', difficulty: 'Fácil', link: 'https://beafn28.gitbook.io/beafn28/writeups/tryhackme/pickle-rick' },
   { title: 'Billing', platform: 'TryHackMe', os: 'Linux', difficulty: 'Fácil', link: 'https://beafn28.gitbook.io/beafn28/writeups/tryhackme/billing' },
